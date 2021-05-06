b7622b2d2b4a53ff179763a57512552ef9acf030
CaptainDrakken <qwertymagicks@gmail.com>
2021-05-06 15:08:38 -0400

    aaa

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+//Get the button
 var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -15,6 +19,8 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
